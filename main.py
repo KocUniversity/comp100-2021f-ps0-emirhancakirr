2657e4bacd73d54e7f9b2dedfe3bfd0f8aef84c2
emirhancakirr <91738804+emirhancakirr@users.noreply.github.com>
2021-10-01 14:01:22 +0000

    Part 1 assignmest done

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+IDnumber = 76409
+numberx = input("Enter number x:")
+numbery = input ("Enter number y:")
+numbery = int(numbery)
+numberx = int(numberx)
+
+print ("x*y=", numberx*numbery)
+print( np.log2(numberx))
+print(IDnumber)
